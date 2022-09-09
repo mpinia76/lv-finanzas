fd37fa6d02237173e16e0fd723bb4740df24fe57
mpinia76 <marcos.pinero1976@gmail.com>
2022-09-09 13:42:44 -0300

    varios

--- resources/views/vendor/adminlte/auth/login.blade.php
+++ resources/views/vendor/adminlte/auth/login.blade.php
@@ -37,14 +37,14 @@
                         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                     </div>
                     <div class="row">
-                        <div class="col-xs-8">
+                        <div class="col-xs-7">
                             <div class="checkbox icheck">
                                 <label>
                                     <input style="display:none;" type="checkbox" name="remember"> {{ trans('adminlte_lang::message.remember') }}
                                 </label>
                             </div>
                         </div><!-- /.col -->
-                        <div class="col-xs-4">
+                        <div class="col-xs-5">
                             <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('adminlte_lang::message.buttonsign') }}</button>
                         </div><!-- /.col -->
                     </div>
